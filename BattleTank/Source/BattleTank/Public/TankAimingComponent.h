3644efeb9ff58877b4bafbc1807979a271cdf043
DESKTOP-KU09RLL\aissg <ben95ump45@gmail.com>
2017-08-29 15:07:32 +0200

    60_Adding TickComponent() Back

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -44,6 +44,10 @@ private:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
+	virtual void BeginPlay() override;
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
 	UTankBarrel* Barrel = nullptr;
 	UTankTurret* Turret = nullptr;
 
